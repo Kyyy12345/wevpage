a8d7c51eba48eed4d17bbde9f293de23ba1da266
Kyyy12345 <127706140+Kyyy12345@users.noreply.github.com>
2023-04-11 16:03:05 +0900

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Result <br>
+box area = <?php echo $_POST["width"]*$_POST["height"]; ?>
+triangle area = <?php echo ($_POST["width"]*$_POST["height"])/2; ?>
+circle area = <? php echo $_POST["circle"]^2 * 3.14;?>
+
+</body>
+</html>
