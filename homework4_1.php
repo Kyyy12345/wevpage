15fb4133a1cff69156f6cd78eda7e95b93922f89
Kyyy12345 <127706140+Kyyy12345@users.noreply.github.com>
2023-04-14 22:32:59 +0900

    Add files via upload

---
+++ homework4_1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+Result<br>
+triangle area = <?php echo ($_POST["width"]*$_POST["height"])/2; ?> <br>
+box area = <?php echo $_POST["width"]*$_POST["height"]; ?> <br>
+
+
+</body>
+</html>
